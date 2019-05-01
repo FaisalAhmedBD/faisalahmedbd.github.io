ca553af1587692eee5610b30edfd2c4ee0e118cf
Faisal Ahmed <33577124+FaisalAhmedBD@users.noreply.github.com>
2019-05-02 01:28:06 +0600

    Updates

---
+++ precache-manifest.e671213cb9b572b6f227a1daf7853ac2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bea0b3b02913c41d43ff84f6610e5e10",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d23ae97d74416c00618a",
+    "url": "/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "d23ae97d74416c00618a",
+    "url": "/static/js/main.28ec169c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
