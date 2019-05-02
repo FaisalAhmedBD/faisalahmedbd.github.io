0b816d0b0425daa93fde288dc81de99e54dd97e0
Faisal Ahmed <33577124+FaisalAhmedBD@users.noreply.github.com>
2019-05-03 00:27:31 +0600

    initiaed header menu

--- src/components/home.js
+++ src/components/home.js
@@ -7,9 +7,28 @@ export default () => {
         <div className="homepage">
             <div className="header container-fluid">
                 <div className="row">
-                    <div className="col-xs-4 col-sm-3 col-md-2">
-                        <h1>Hello world</h1>
+                    <div className="container">
+                        <div className="header-menu">
+                            <div className="row">
+                                <div className="col-xs-4 col-sm-3 col-md-2">
+                                    <div className="social-share">
+                                        <ul>
+                                            <li>  facebook   </li>
+                                            <li>  LinkedIn   </li>
+                                            <li>  Gmail   </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div className="col-xs-4 col-sm-6 col-md-8 text-center">
+                                    <h1>Hello world</h1>
+                                </div>
+                                <div className="col-xs-4 col-sm-3 col-md-2 text-right">
+                                    <h1>Hello world</h1>
+                                </div>
+                            </div>
+                        </div>
                     </div>
+
                 </div>
             </div>
         </div>
